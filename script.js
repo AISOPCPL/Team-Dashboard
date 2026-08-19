def465c58facbc70f4ebf8c2c952d5a240f1a701
Avaniysh Interior Solutions OPC Pvt. Ltd. <systems.aispl@gmail.com>
2026-08-19 10:16:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -274,7 +274,13 @@ const employeesData = [
         name: 'Insurance Checklist', 
         docId: '1g6ZEWY32KmJlRhoBoxdEolQP_SPjwDbLlWCvmy7vPuk', 
         gid: '2092959791', 
-        selectedColumns: ['Name','Task', 'Doer', 'Planned', 'Actual', 'Status'] 
+        selectedColumns: ['Task', 'Doer', 'Planned', 'Actual', 'Status'] 
+      },
+      { 
+        name: 'Checklist', 
+        docId: '1m_P5_eP9FvD9eaYvanmLlV833cscTvk2xJk_c39cbZw', 
+        gid: '2092959791', 
+        selectedColumns: ['Task', 'Doer', 'Planned', 'Actual', 'Status'] 
       },
       { 
         name: 'Delegation Sheet Team',
@@ -294,13 +300,7 @@ const employeesData = [
     ],
  
     sheets: [
-      { name: 'Office Expenses',formUrl:'https://docs.google.com/forms/d/e/1FAIpQLSeFG1ZBY4mstlGPDpcNhUb9vi2WiJGBr3aG2cuTtYqPNXf4Nw/viewform?usp=header', docId: '1N2DF6vzSoJcIxwL2makablsGtDlMB-rxhcWT3_VLHBY', gid: '440445714', selectedColumns: ['Date', 'Paid to', 'Amount', 'Reason', 'Categories'] },
-      { 
-        name: 'Checklist', 
-        docId: '1m_P5_eP9FvD9eaYvanmLlV833cscTvk2xJk_c39cbZw', 
-        gid: '2092959791', 
-        selectedColumns: ['Name','Task', 'Doer', 'Planned', 'Actual', 'Status'] 
-      }
+      { name: 'Office Expenses',formUrl:'https://docs.google.com/forms/d/e/1FAIpQLSeFG1ZBY4mstlGPDpcNhUb9vi2WiJGBr3aG2cuTtYqPNXf4Nw/viewform?usp=header', docId: '1N2DF6vzSoJcIxwL2makablsGtDlMB-rxhcWT3_VLHBY', gid: '1456510666', selectedColumns: ['Date', 'Paid to', 'Amount', 'Reason', 'Categories'] }
     ]
   },
   {
@@ -388,7 +388,17 @@ document.addEventListener('DOMContentLoaded', () => {
     userNameElem.innerText = userName;
   }
 
-  const role = sessionStorage.getItem('loggedInUserRole');
+ const role = sessionStorage.getItem('loggedInUserRole');
+const systemNavBtn = document.querySelector('.nav-btn[data-target="system-view"]'); 
+
+if (systemNavBtn) {
+  if (role !== 'admin') {
+    systemNavBtn.style.display = 'none'; 
+  } else {
+    systemNavBtn.style.display = 'flex'; 
+  }
+}
+
   const accessIds = JSON.parse(sessionStorage.getItem('accessIds') || '[]');
   let displayData = employeesData;
 
@@ -1064,3 +1074,18 @@ function parseGvizDate(cell) {
 
   return null;
 }
+
+
+
+ document.addEventListener("DOMContentLoaded", function () {
+      if (typeof renderEmployees === 'function') {
+        renderEmployees();
+      } else if (typeof initEmployeePage === 'function') {
+        initEmployeePage();
+      }
+    });
+
+    function logout() {
+      sessionStorage.removeItem('isLoggedIn');
+      window.location.href = 'login.html';
+    }
